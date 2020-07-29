47e0c20ee8fb2ee89d6248045c0d4c48493341e6
Administrator <Administrator@A068>
2020-07-29 20:34:40 +0800

    1

--- XyCloud-config/src/main/java/xyz/xy718/config/XyCloudConfigApplication.java
+++ XyCloud-config/src/main/java/xyz/xy718/config/XyCloudConfigApplication.java
@@ -2,8 +2,10 @@ package xyz.xy718.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class XyCloudConfigApplication {
 
     public static void main(String[] args) {
