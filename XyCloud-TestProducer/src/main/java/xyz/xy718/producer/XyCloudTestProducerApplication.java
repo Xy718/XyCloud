288f41c4c8f14885fdf3bce6565fc4cf88c5eb54
Xy718 <869839000@qq.com>
2020-07-30 14:07:35 +0800

    1

--- XyCloud-TestProducer/src/main/java/xyz/xy718/producer/XyCloudTestProducerApplication.java
+++ XyCloud-TestProducer/src/main/java/xyz/xy718/producer/XyCloudTestProducerApplication.java
@@ -3,6 +3,8 @@ package xyz.xy718.producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Data;
+
 @SpringBootApplication
 public class XyCloudTestProducerApplication {
 
